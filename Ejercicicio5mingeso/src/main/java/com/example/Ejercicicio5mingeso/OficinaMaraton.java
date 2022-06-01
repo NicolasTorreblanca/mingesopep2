7cfa7013c7c77512b5bac76655a527dc06a52574
NicolasTorreblanca <nicolas.torreblanca@usach.cl>
2022-06-01 12:06:15 -0400

    primer commit

---
+++ Ejercicicio5mingeso/src/main/java/com/example/Ejercicicio5mingeso/OficinaMaraton.java
@@ -0,0 +1,25 @@
+package com.example.Ejercicicio5mingeso;
+
+import java.util.*;
+
+public class OficinaMaraton {
+
+
+    private ArrayList<Corredor> listaCorredores;
+
+    OficinaMaraton(){
+        this.listaCorredores =  new ArrayList() ;
+    }
+
+    public void agregarUsuario(Corredor corredor){
+        this.listaCorredores.add(corredor);
+    }
+    
+
+   // calcularCorredorPrimerPuestoMaraton(){
+
+        
+
+}
+   
+
---
+++ Ejercicicio5mingeso/src/test/java/com/example/Ejercicicio5mingeso/Ejercicicio5mingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ejercicicio5mingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicicio5mingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
